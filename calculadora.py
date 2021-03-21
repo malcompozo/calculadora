0a355e857124c37312ee170294651f02cb46ee50
Malcom Pozo <malcompozo@gmail.com>
2021-03-21 20:31:10 -0300

    Creacion de la ventana

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,9 @@
+# -----------> creada por Malcom Pozo <----------- #
+
+from tkinter import * #importar la libreria
+
+#ventana 
+ventana = Tk() #interfaz
+ventana.title("Calculadora") #configura el titulo de la ventana
+ventana.config(backgroud="lightblue") #configura el color de fondo de la ventana
 
